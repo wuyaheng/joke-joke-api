4897857f422caf0fec6e963af3e112216ca1d170
wuyaheng <wuyaheng2016@gmail.com>
2020-06-16 13:04:27 -0400

    set up and add title

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <React.Fragment>
+      <h1 className="text-center mt-3"><span role="img" aria-label="Joy">😂</span>Joke Joke<span role="img" aria-label="Joy">😂</span></h1>
+    </React.Fragment>
+  );
+}
+
+export default App;
